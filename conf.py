466ee26721189de84a342e5cd98ffbe6b842a0d9
seyoung <seyoung@aachons-Mac-mini.local>
2018-07-20 00:47:38 +0900

    Remove nbsphinx

--- conf.py
+++ conf.py
@@ -32,7 +32,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.ifconfig',
     'sphinx.ext.mathjax',
-    'nbsphinx',
+#    'nbsphinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
